4b1a45524c83c765c2840a539670330f37018e0f
Merill Fernando <merill@merill.net>
2024-03-03 18:27:03 +1100

    Update info-cards.tsx

--- app/message/[id]/components/info-cards.tsx
+++ app/message/[id]/components/info-cards.tsx
@@ -34,8 +34,8 @@ export default function InfoCards(props: { id: string }) {
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
 
 
-                <a href={`https://admin.microsoft.com/#/MessageCenter/:/messages/${msg?.Id}`} target="_blank" rel="noopener">
-                    <Card className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-md">
+                <Card className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-md">
+                    <a href={`https://admin.microsoft.com/#/MessageCenter/:/messages/${msg?.Id}`} target="_blank" rel="noopener">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                             <CardTitle className="text-sm font-medium">
                                 Message ID
@@ -58,8 +58,9 @@ export default function InfoCards(props: { id: string }) {
                             <p className="text-xs text-muted-foreground">
                             </p>
                         </CardContent>
-                    </Card>
-                </a>
+                    </a>
+                </Card>
+
 
                 <Card className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-md">
                     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -156,28 +157,56 @@ export default function InfoCards(props: { id: string }) {
                 </Card>
                 {roadmapId && (
                     <Card className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-md">
-                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                                Roadmap ID
-                            </CardTitle>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="h-4 w-4 text-muted-foreground"
-                            >
-                                <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                            </svg>
-                        </CardHeader>
-                        <CardContent>
-                            <a href={`https://www.microsoft.com/en-US/microsoft-365/roadmap?filters=&searchterms=${roadmapId}`} target="_blank" rel="noopener" ><div className="text-2xl font-bold">{roadmapId}</div></a>
-                            <p className="text-xs text-muted-foreground">
-                            </p>
-                        </CardContent>
+                        <a href={`https://www.microsoft.com/en-US/microsoft-365/roadmap?filters=&searchterms=${roadmapId}`} target="_blank" rel="noopener" >
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Roadmap ID
+                                </CardTitle>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    className="h-4 w-4 text-muted-foreground"
+                                >
+                                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                                </svg>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{roadmapId}</div>
+                                <p className="text-xs text-muted-foreground">
+                                </p>
+                            </CardContent>
+                        </a>
+                    </Card>
+                )}
+                {dateActBy && (
+                    <Card className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-md">
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Act by
+                                </CardTitle>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    className="h-4 w-4 text-muted-foreground"
+                                >
+                                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                                </svg>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">{dateActBy}</div>
+                                <p className="text-xs text-muted-foreground">
+                                </p>
+                            </CardContent>
                     </Card>
                 )}
             </div>
