c993b5313988a417711b6da98a9737c739effe01
Jed Laundry <jlaundry@jlaundry.com>
2025-08-27 02:11:14 +0000

    Add /rss.xml route

---
+++ app/rss.xml/route.tsx
@@ -0,0 +1,36 @@
+
+import { getAllMessages, getMessageSummary } from "@/lib/messages";
+import RSS from "rss";
+
+export async function GET() {
+    const dataMessages = getAllMessages();
+
+    const feedOptions = {
+        title: "Microsoft 365 Message Center Archive",
+        description: "This site is an archive of Microsoft 365 Message Center messages for quick reference.",
+        site_url: `https://mc.merill.net`,
+        feed_url: `https://mc.merill.net/rss`,
+        pubDate: new Date(),
+    };
+
+    const feed = new RSS(feedOptions);
+
+    dataMessages.map((item) => {
+        const lastModifiedDateTime = new Date(item.LastModifiedDateTime ?? Date.now());
+        feed.item({
+            title: `${item.Id}: ${item.Title}`,
+            description: getMessageSummary(item),
+            url: `https://mc.merill.net/message/${item.Id}`,
+            date: lastModifiedDateTime,
+        });
+    });
+
+    return new Response(
+        feed.xml({ indent: true }),
+        {
+            headers: {
+                "Content-Type": "application/rss+xml;charset=utf-8",
+            },
+        },
+    );
+}
